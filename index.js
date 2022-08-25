61687d27b4738ead87b35e4e64101ebee28dc4a6
Miguel Angel <eagleangel@Mac-Studio-de-Miguel.local>
2022-08-24 19:53:19 -0500

    testing deploy

---
+++ index.js
@@ -0,0 +1,14 @@
+var http = require('http');
+
+iniciar();
+
+function iniciar() {
+  function onRequest(_, response) {
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write('Hola Mundo con NodeJS');
+    response.end();
+  }
+  const port = process.env.PORT || 8080;
+  http.createServer(onRequest).listen(port);
+  console.log('database connected on port', port);
+}
